a7f4db762409dee9281c1e82c42a7897838153a7
Alauddin33 <shamem33@gmail.com>
2022-11-27 17:55:57 +0600

    category data api created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -12,14 +14,45 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.6owueqa.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6owueqa.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const carDetailsCollection = client.db('carPolli').collection('carCollections');
+        const usersCollection = client.db('carPolli').collection('users');
+
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { category_id: id };
+            const categoryDetails = await carDetailsCollection.find(query).toArray();
+            res.send(categoryDetails);
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            }
+            const result = usersCollection.updateOne(filter, updateDoc, options);
+            console.log(result);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d',
+            })
+            console.log(token);
+            res.send(result, token)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 
 app.get('/', async (req, res) => {
